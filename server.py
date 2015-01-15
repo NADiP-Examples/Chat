d1dead6c11f9c44aedcf10429d6f7739082910df
BoobleGum <booblegum42@gmail.com>
2015-01-16 03:26:52 +0700

    v.0.2

--- server.py
+++ server.py
@@ -1,43 +1,52 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import socket, threading
 
-import time
-import socket
+IP = ""
+PORT = 14900
 
-def parse(conn):# обработка соединения в отдельной функции
-    data = conn.recv(1024)
-    return data
+class Connect(threading.Thread):
+    def __init__(self, sock, addr):
+        self.sock = sock
+        self.addr = addr
+        threading.Thread.__init__(self)
+        print(self.addr,"...connected")
+        ListClients[self.addr] = self.sock
 
+    def run (self):
+        while 1:
+            try:
+                buf = self.sock.recv(2048)
+                print ("!!!",self.addr,buf)
+                print("data send")
+                self.send_message_to_all(buf)
+
+            except socket.error:
+                # print(self.addr[0])
+                # print(ListClients[self.addr][1])
+                msg = "disconnected"
+                print(msg)
+                del ListClients[self.addr]
+                print("all Clients = ", ListClients)
+                break
+
+    def send_message_to_all(self, message):
+        for client in ListClients.values():
+            print(message)
+            print('client = ', client)
+            client.send(message)
+
+#Список клиентов
+ListClients = {}
 
 sock = socket.socket()
-sock.bind(("", 14900))
+sock.bind((IP, PORT))
 sock.listen(4)
-sock.setblocking(0)
 connects = [] #Все клиентские подлючения
 
 
 while True:
-    try:
-        conn, addr = sock.accept()
-        connects.append(conn)
-        print('connect: %s. Num connected: %s'%(addr, len(connects)))
-    except socket.error: # данных нет
-        pass # тут ставим код выхода
-    # finally:
-    #     # так при любой ошибке
-    #     # сокет закроем корректно
-    #     print('close')
-    #     sock.close()
-    for conn in connects:
-        try: data = parse(conn)
-        except socket.error: # данных нет
-            pass # тут ставим код выхода
-        else: # данные есть
-            print('data = ',data)
-            for send_conn in connects:
-                if conn == send_conn:
-                    continue
-                print('data sending')
-                send_conn.send(data.upper())
+    conn, addr = sock.accept()
+    Connect(conn, addr).start()
 
 
